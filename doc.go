ed1acb91058424f18022ccc78da00eba09c9e295
Bestony <bestony@users.noreply.github.com>
2017-03-14 22:41:21 +0800

    Create doc.go

---
+++ doc.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Usage() {
+	fmt.Println("Usage: rss2md [link]")
+	fmt.Println()
+	fmt.Println("Read Link From Feed URL and Make a　Markdown　File")
+	fmt.Println()
+
+}
